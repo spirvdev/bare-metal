8598f51e62c87e9c3dfab65653ef066b68767a42
Gabriel Correia <gabrielcorreia01012020@hotmail.com>
2022-10-06 10:08:48 -0400

    Project created with c++ support

---
+++ app/src/main/java/com/beloncode/baremetal/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.beloncode.baremetal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.beloncode.baremetal.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    companion object {
+        init {
+            System.loadLibrary("bm")
+        }
+    }
+}
